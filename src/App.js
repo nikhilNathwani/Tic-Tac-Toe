9d3cf42c4dd53a45df7d2b768608ca79f91f1efa
nikhilNathwani <nnathwani36@gmail.com>
2023-09-06 12:47:34 -0700

    Completed tutorial, added Time Travel functionality

--- src/App.js
+++ src/App.js
@@ -11,6 +11,51 @@ Questions:
 import React from "react";
 import { useState } from "react";
 
+export default function Game() {
+	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const [currentMove, setCurrentMove] = useState(0);
+	const xIsNext = currentMove % 2 === 0;
+	const currentSquares = history[currentMove];
+
+	function handlePlay(nextSquares) {
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+		setHistory(nextHistory);
+		setCurrentMove(nextHistory.length - 1);
+	}
+
+	function jumpTo(nextMove) {
+		setCurrentMove(nextMove);
+	}
+
+	const moves = history.map((squares, move) => {
+		let description;
+		if (move > 0) {
+			description = `Go to move #${move}`;
+		} else {
+			description = "Restart game";
+		}
+		return (
+			<li key={move}>
+				<button onClick={() => jumpTo(move)}>{description}</button>
+			</li>
+		);
+	});
+	return (
+		<div className="game">
+			<div className="game-board">
+				<Board
+					xIsNext={xIsNext}
+					squares={currentSquares}
+					onPlay={handlePlay}
+				/>
+			</div>
+			<div className="game-info">
+				<ol>{moves}</ol>
+			</div>
+		</div>
+	);
+}
+
 function Square({ value, onSquareClick }) {
 	return (
 		<button className="square" onClick={onSquareClick}>
@@ -19,18 +64,14 @@ function Square({ value, onSquareClick }) {
 	);
 }
 
-export default function Board() {
-	const [xIsNext, setXIsNext] = useState(true);
-	const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
 	function handleClick(i) {
 		if (squares[i] || calculateWinner(squares)) {
 			return;
 		}
 		const nextSquares = squares.slice();
 		nextSquares[i] = xIsNext ? "X" : "O";
-		setSquares(nextSquares);
-		setXIsNext(!xIsNext);
+		onPlay(nextSquares);
 	}
 
 	const winner = calculateWinner(squares);
